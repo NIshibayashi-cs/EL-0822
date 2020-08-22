027bc886706f388219f1b48084a4162f4c2f3dbf
Goto-Tatsu <50894018+Goto-Tatsu@users.noreply.github.com>
2020-08-22 12:30:44 +0900

    BuildSettingに、基本的なシーン遷移をする『title』『result』を追加。 『title』シーンからは、一時的にSampleSceneを呼び出すように書いています。
    Signed-off-by: Goto-Tatsu <50894018+Goto-Tatsu@users.noreply.github.com>

---
+++ El-0822/Assets/Goto_work/Scripts/GoToScene_Title.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class GoToScene_Title : MonoBehaviour
+{
+    private string titleSceneName = "title";
+    // Start is called before the first frame update
+    public void OnClick()
+    {
+        Debug.Log("GoToTitleButton クリック。");
+
+        // ボタンの色を変える
+        gameObject.GetComponent<Image>().color = Color.cyan;
+
+        // ゲームシーンの非同期ロード開始    
+        Debug.Log("ゲームシーンの非同期読込開始。");
+        SceneManager.LoadSceneAsync(titleSceneName);
+    }
+}
