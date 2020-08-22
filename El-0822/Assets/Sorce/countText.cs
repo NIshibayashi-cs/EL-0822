f7a7a4c1afa19dcff1effe3b3b7bcb1c47df7912
nagami <55968582+RamneChan@users.noreply.github.com>
2020-08-22 14:39:26 +0900

    nagami
    game

---
+++ El-0822/Assets/Sorce/countText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class countText : MonoBehaviour
+{
+    private int count_;  // 正解カウント
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count_ = 0;
+        gameObject.GetComponent<Text>().text = "" + count_;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // カウントアップ
+    public void CountUp()
+    {
+        count_++;
+        gameObject.GetComponent<Text>().text = "" + count_;
+    }
+
+    // カウントダウン
+    public void CountDown()
+    {
+        count_--;
+        gameObject.GetComponent<Text>().text = "" + count_;
+    }
+}
---
+++ El-0822/Assets/Sorce/hanntei.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hanntei : MonoBehaviour
+{
+    public int frameCount_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        frameCount_ = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(frameCount_ == 180)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+        frameCount_++;
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // 一文字とチェックを照合
+        if(other.gameObject.GetComponent<obj>().ichimoji_ == other.gameObject.GetComponent<obj>().check_)
+        {
+            transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.GetComponent<mark>().Seikai();
+            transform.GetChild(1).gameObject.transform.GetChild(0).gameObject.GetComponent<countText>().CountUp();
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.transform.GetChild(0).gameObject.GetComponent<mark>().Husekikai();
+        }
+
+        // ふきだしだす
+        transform.GetChild(0).gameObject.SetActive(true);
+        frameCount_ = 0;
+
+        //当たったオブジェクトを消す
+        Destroy(other.gameObject);
+    }
+}
