9620543b43b30551ba45ea3d044b7580138d76a8
nagami <55968582+RamneChan@users.noreply.github.com>
2020-08-22 15:56:57 +0900

    nagami
    nagami

---
+++ El-0822/Assets/Sorce/timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public float countTime_;
+    public int time_;
+    public GameObject count_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countTime_ = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countTime_ += Time.deltaTime;
+        if(countTime_ >= time_)
+        {
+            gameObject.GetComponent<Text>().text = "0";
+            PlayerPrefs.SetInt("Score", count_.GetComponent<countText>().GetCount());
+            SceneManager.LoadSceneAsync("result");
+
+        }
+        gameObject.GetComponent<Text>().text = "" + (time_ - (int)countTime_);
+    }
+}
